8dd4a73722715f9d3b0f1bf850dc67b577f65279
Felisa Deang <felisadee@gmail.com>
2016-08-17 12:23:34 -0700

    update querying for unlike to remove use of helper method

--- app/models/secret.rb
+++ app/models/secret.rb
@@ -4,7 +4,7 @@ class Secret < ActiveRecord::Base
   has_many :users_liked, through: :likes, source: :user
   validates :content, presence: true
 
-  def find_like_id current_user
-      Like.select(:id).where(user: current_user, secret: self)[0].id
-  end
+  # def find_like_id current_user
+  #     Like.select(:id).where(user: current_user, secret: self)[0].id
+  # end
 end
